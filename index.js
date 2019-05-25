6e11c0364f4a65ab88c9d3c1abb75f3d0368b882
Allison Howard <alliemhow@gmail.com>
2019-05-25 23:25:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const newID = 1000000000 + oldID;
 
 const ageIsValid = Number.isInteger(currentAge);
 
-const randomNumber = Math.floor(Math.random() * 20) + 1;
+const randomNumber = Math.random() * 20;
 
-const randomInteger = Math.floor(randomNumber)
+const randomInteger = Math.floor(randomNumber);
+
+randomUserID = randomInteger + 1000000001;
